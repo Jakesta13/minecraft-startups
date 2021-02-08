f7c91d794e5ae86e83d66de946db49c88a3c74f4
jakesta13 <10874330+Jakesta13@users.noreply.github.com>
2021-02-08 11:39:16 -0500

    Some more code updates for control panel use...

--- uhc-start.sh
+++ uhc-start.sh
@@ -2,14 +2,14 @@
 # Add direct downloads to plugins you wish to download to "plugins.txt" and this script will download them.
 ### ### Settings ### ###
 # Allow auto updates of paper?
-paperupdate="${1:-TRUE}"
+paperupdate="${1:-true}"
 # Paper Minecraft Version
 mcver="${2:-1.16.5}"
 # Server jar name
 jar="${3:-paperclip.jar}"
 
 # Allow Auto-updates of UhcCore?
-UHCupdate="${4:-TRUE}"
+UHCupdate="${4:-true}"
 # Lock to Specific Version?
 # Leave Blank to always get latest.
 UHCver="${5}"
@@ -59,6 +59,12 @@ if [ ! "${fullarguments}" ]; then
 	printf "Syntax: \n./uhc-start.sh paperupdate(y/n) mcver(1.16.5) jar(paperclip.jar) UHCupdate(y/n) UHCver(v1.18) Xmx(1024) Xms(1024) args(nogui) eula(false) levelseed(1234) world(world) view-distance(10) port(25565) whitelist(false) max-players(20) block-proxy-connections(true) player-tracking-range(256)\n"
 	exit 1
 fi
+## ## Functions ## ##
+# Removes all double-quotes from any input
+fixquotes () {
+sed -e 's/"//g'
+}
+## ## ## ##
 #### #### Setup #### ####
 # This is to set up the directories and files required for a clean startup.
 if [ ! -d "plugins" ] && [ ! -e "eula.txt" ] && [ ! -e "server.properies" ] && [ ! -e "spigot.yml" ]; then
@@ -67,21 +73,19 @@ if [ ! -d "plugins" ] && [ ! -e "eula.txt" ] && [ ! -e "server.properies" ] && [
 	echo "eula=${eula}" > "eula.txt"
 # Setting up server.properties, if file exists, we will skip this.
 	if [ ! -e "server.properties" ]; then
-		printf "level-seed=${levelseed}\nlevel-name=${world}\nview-distance=${distance}\nserver-port=${port}\nwhite-list=${whitelist}\nmax-players=${maxp}\nprevent-proxy-connections=${proxy}" > "server.properties"
-		printf "    entity-tracking-range:\n      players: ${prange}" > "spigot.yml"
-sed -i 's/"//g' "server.properties"
+		printf "level-seed=${levelseed}\nlevel-name=${world}\nview-distance=${distance}\nserver-port=${port}\nwhite-list=${whitelist}\nmax-players=${maxp}\nprevent-proxy-connections=${proxy}" | fixquotes > "server.properties"
+		printf "    entity-tracking-range:\n      players: ${prange}" | fixquotes > "spigot.yml"
 	fi
-	else
+else
 	# if files exist, then just update the configs.
-	sed -i -e "s/level-seed=.*$/level-seed=${levelseed}/" -e "s/level-name=.*$/level-name=${world}/" -e "s/view-distance=.*$/view-distance=${distance}/" -e "s/server-port=.*$/server-port=${port}/" -e "s/white-list=.*$/white-list=${whitelist}/" -e "s/max-players=.*$/max-players=${maxp}/" -e "s/prevent-proxy-connections=.*$/prevent-proxy-connections=${proxy}/" "server.properties"
-	sed -i -e "s/players: .*$/players: ${prange}/" "spigot.yml"
-	sed -i -e "s/eula=.*/eula=${eula}/" "eula.txt"
-sed -i 's/"//g' "server.properties"
+	sed -i -e "s/level-seed=.*$/level-seed=${levelseed}/" -e "s/level-name=.*$/level-name=${world}/" -e "s/view-distance=.*$/view-distance=${distance}/" -e "s/server-port=.*$/server-port=${port}/" -e "s/white-list=.*$/white-list=${whitelist}/" -e "s/max-players=.*$/max-players=${maxp}/" -e "s/prevent-proxy-connections=.*$/prevent-proxy-connections=${proxy}/" -e 's/"//g' "server.properties"
+	sed -i -e "s/players: .*$/players: ${prange}/" -e 's/"//g' "spigot.yml"
+	sed -i -e "s/eula=.*/eula=${eula}/" -e 's/"//g' "eula.txt"
 fi
 #### #### #### ####
 
 # Download paper, if allowed to
-if [ "${paperupdate}" == "TRUE" ]; then
+if [ "${paperupdate}" == "true" ]; then
 	# If paperclip.jar does not exist, download normally.
 	if [ ! -e "${jar}" ]; then
 		wget --quiet "https://papermc.io/api/v1/paper/${mcver}/latest/download" -O "${jar}"
@@ -92,7 +96,7 @@ if [ "${paperupdate}" == "TRUE" ]; then
 fi
 
 # Download UHC, if allowed to
-if [ "${UHCupdate}" == "TRUE" ]; then
+if [ "${UHCupdate}" == "true" ]; then
 	# If we are not locked to one specific version of the plugin, then lets grab the latest.
 	if [ ! "${UHCver}" ]; then
 		# Curl from https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c#gistcomment-2552690
@@ -129,7 +133,7 @@ sleep 0.5
 # Finally ... Start the server.
 # If no Xms or Xmx Arguments ... fail!
 if [ ! "${Xmx}" ] || [ ! "${Xms}" ]; then
-	echo "You did not specify a memory allocations, You need to make sure -Xms and -Xmx are set!"
+	echo "You did not specify any memory allocations, You need to make sure -Xms and -Xmx are set!"
 else
 	# Now check if the memory allocation contains letters .. if so then fail.
 	checkvar=$(echo "${Xms} ${Xmx}" | grep [^0-9])

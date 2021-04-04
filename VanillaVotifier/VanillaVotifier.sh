76b62b6f9c926b07346f5817b39114efec85daeb
jakesta13 <10874330+Jakesta13@users.noreply.github.com>
2021-04-04 13:15:40 -0400

    More Typos.. more fixes

--- VanillaVotifier/VanillaVotifier.sh
+++ VanillaVotifier/VanillaVotifier.sh
@@ -2,7 +2,7 @@
 ## Settings ##
 # See VanillaVotifer.conf
 if [ ! -e "VanillaVotifier.conf" ]; then
-	wget --quiet "https://github.com/Jakesta13/minecraft-startups/raw/master/VanillaVotifier/VanillaVotifier.conf"
+	wget -c -N --quiet "https://github.com/Jakesta13/minecraft-startups/raw/master/VanillaVotifier/VanillaVotifier.conf"
 fi
 . VanillaVotifer.conf
 if [ ! -e "SecondConf.conf" ]; then
@@ -12,7 +12,7 @@ fi
 ## Functions ##
 # No Touchie #
 function download {
-wget -N --quiet "${updateUrl}" -O "${fileName}"
+wget -c -N --quiet "${updateUrl}" -O "${fileName}"
 }
 # Update / Download stage
 if [ "${update}" == "true" ]; then
@@ -22,7 +22,7 @@ if [ "${update}" == "true" ]; then
 	fileName="VanillaVotifier.jar"
 	download
 else
-	if [ ! -z "${setVer}" ]; then
+	if [ -n "${setVer}" ]; then
 		echo "Downloading V${setVer}"
 		setVer=$(echo "${setVer}" | sed -e 's/V//g' -e 's/v//g')
 		updateUrl="https://github.com/xMamo/VanillaVotifier/releases/download/${setVer}/VanillaVotifier.jar"
